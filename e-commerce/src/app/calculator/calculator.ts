b3643cbd1451454122f2db74e60c1b87a55b3f8b
sudakar2tech <sudakar2tech@gmail.com>
2026-06-10 19:14:54 +0530

    calculator

--- e-commerce/src/app/calculator/calculator.ts
+++ e-commerce/src/app/calculator/calculator.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,8 +9,26 @@ import { FormsModule } from '@angular/forms';
 })
 export class Calculator {
 
-fnOne() {
-  alert('clicked')
- 
-}
+ display = signal<string>('0');
+
+  // Append clicked numbers or operators
+  appendValue(val: string) {
+    this.display.update(current => current === '0' ? val : current + val);
+  }
+
+  // Clear the display
+  clear() {
+    this.display.set('0');
+  }
+
+  // Calculate the result
+  calculate() {
+    try {
+      // Evaluate the mathematical expression safely
+      const result = eval(this.display());
+      this.display.set(result.toString());
+    } catch (error) {
+      this.display.set('Error');
+    }
+  }
 }
--- e-commerce/src/main.ts
+++ e-commerce/src/main.ts
@@ -4,7 +4,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 //import { App } from './app/app';
 //import { Login } from './app/login/login';
-import { Registration } from './app/registration/registration';
-//import { Calculator } from './app/calculator/calculator';
+//import { Registration } from './app/registration/registration';
+import { Calculator } from './app/calculator/calculator';
 
-bootstrapApplication(Registration, appConfig).catch((err) => console.error(err));
+bootstrapApplication(Calculator, appConfig).catch((err) => console.error(err));
